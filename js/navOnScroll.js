a03779a4819f788f50bdb988dd74d78a149fa04c
danbender <mail@danbender.net>
2014-01-30 12:19:05 +0100

    put js in one file, small refactor

--- js/navOnScroll.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function(){
-  if ($("body").scrollTop() > 150) {
-    $("#actionbar").addClass("shrinkActionBar");
-    $(".menu").addClass("shrinkMenu");
-  } else {
-    $("#actionbar").removeClass("shrinkActionBar");
-    $(".menu").removeClass("shrinkMenu");
-  }
-});
